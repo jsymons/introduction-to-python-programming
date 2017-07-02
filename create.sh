2a6737e00f998ecdafaa0ba6348894adf90eda73
Santiago Basulto <santiago.basulto@gmail.com>
2017-07-02 08:47:34 -0400

    Reading lessons created

--- create.sh
+++
@@ -1,6 +0,0 @@
-rmotr_curriculum_tools create_lesson unit-1-getting-started "Introduction" -t reading
-rmotr_curriculum_tools create_lesson unit-2-collections "Introduction" -t reading
-rmotr_curriculum_tools create_lesson unit-3-advanced-collections "Introduction" -t reading
-rmotr_curriculum_tools create_lesson unit-4-modules-exceptions "Introduction" -t reading
-rmotr_curriculum_tools create_lesson unit-5-intro-to-oop "Introduction" -t reading
-rmotr_curriculum_tools create_lesson unit-6-advanced-oop "Introduction" -t reading
