743f0e007c51920275f43a337e1675101c9ac577
jasonmeeks <jasonmeeks42@gmail.com>
2017-08-08 19:58:33 -0500

    Update solution_.py

--- unit-5-intro-to-oop/lesson-4-make-a-car-with-attributes/solutions/solution_.py
+++ unit-5-intro-to-oop/lesson-4-make-a-car-with-attributes/solutions/solution_.py
@@ -3,10 +3,10 @@ class Car(object):
 
 car1 = Car()
 car1.color = 'blue'
-car1.maker = 'Tesla'
+car1.make = 'Tesla'
 car1.model = 'Model S'
 
 car2 = Car()
 car2.color = 'red'
-car2.maker = 'Chevy'
+car2.make = 'Chevy'
 car2.model = 'Camaro'
